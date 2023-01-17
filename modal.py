3c1ed7b98fd3394c9b3743ba1a6174b0b276ca5f
Camilo Italo <alpha19ci@gmail.com>
2023-01-17 15:26:24 -0300

    Add DB and Use Flask

--- modal.py
+++ modal.py
@@ -1,3 +1,10 @@
 from flask import Flask, current_app
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)    
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///exemplo.db'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
